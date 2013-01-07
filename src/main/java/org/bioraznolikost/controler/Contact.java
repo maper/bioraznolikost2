4cac1eb604a59fa65f6d50c9ff22688aecfeebe2
maper <peranic@gmail.com>
2013-01-07 17:20:19 +0100

    test

--- src/main/java/org/bioraznolikost/controler/Contact.java
+++ src/main/java/org/bioraznolikost/controler/Contact.java
@@ -31,7 +31,7 @@ public class Contact  {
     @RequestMapping("contact/send")
 	public ModelAndView send(@ModelAttribute("command")ContactData data, HttpServletRequest req,HttpServletResponse resp){
     	System.out.println("test"+data.getMsg());
-    	ReCaptcha captcha = ReCaptchaFactory.newReCaptcha("my-public-key", "my-private-key", false);
+    	ReCaptcha captcha = ReCaptchaFactory.newReCaptcha("6LcVNdsSAAAAAFCZ2gJ8DO-aj0aEqZyMiUS3T-ij", "6LcVNdsSAAAAAH_X_Sx_GVLpNLjBd5NELAiHnoLN", false);
         ReCaptchaResponse response = captcha.checkAnswer(req.getRemoteAddr(), req.getParameter("recaptcha_challenge_field"), req.getParameter("recaptcha_response_field"));
         ContactData cd=new ContactData();
         if (response.isValid()) {
